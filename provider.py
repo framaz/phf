71f58c4fc0e5ec6591f98e213eb4ef169f902542
framaz <framaz@yandex.ru>
2020-05-26 00:50:21 +0500

    Docstrings, type annotations, some provider architecture revamp and change of ComplexContentProvider.
    ComplexContentProvider is changed to allow it to modify data before sending to hooks and after recieving results from hooks.

--- provider.py
+++ provider.py
@@ -1,7 +1,9 @@
 """Module for data sources.
 
-Now there 3 types of content providers: PeriodicContentProvider, BlockingContentProvider
-and ComplexContentProvider.
+Now there 3 types of content providers for external usage: PeriodicContentProvider,
+BlockingContentProvider and ComplexContentProvider.
+
+PeriodicContentProvider and BlockingContentProvider are subclasses of ConsistentDataProvider.
 
 PeriodicContentProvider is executed every period seconds. It is supposed to be used with fast,
 non-blocking or async data retrieval.
@@ -10,7 +12,8 @@ BlockingContentProvider is executed in cycle lice periodic one, but all waiting 
 the user. As get_content is executed in a different thread, you should not be afraid of doing some
 blocking IO as it won't block the whole system.
 
-Both of them can be easily created by inheriting and overriding following methods:
+As being subclasses of ConsistentDataProvider, both of them can be easily created by inheriting and
+overriding following methods:
 
 - async get_content(self) - function for receiving content, has to be overridden. Returns the data in
 any data form(ie object, dict, str etc). The function's result value is sent to all the hooks. You should
@@ -77,8 +80,6 @@ class AbstractContentProvider:
         obj._is_with_callback = False
 
         # Checking if callbacks are needed.
-        if AbstractContentProvider.result_callback != cls.result_callback:
-            obj._is_with_callback = True
         return obj
 
     def __init__(self, *args, **kwargs):
@@ -91,26 +92,10 @@ class AbstractContentProvider:
         self._asyncio_running = False
         self._asyncio_loop = None
         self._is_with_callback = False
-        if AbstractContentProvider.result_callback != self.__class__.result_callback:
-            self._is_with_callback = True
-
-    async def get_content(self) -> object:
-        """Get content, not implemented.
-
-        Returns:
-            Data in any format.
-            """
-        raise NotImplementedError(f"Get_site of {self.__class__} not overridden")
 
     async def cycle(self) -> None:
         """Main working cycle of provider, not implemented."""
         raise NotImplementedError(f"Cycle of {self.__class__} not overridden")
-
-    async def result_callback(self, results: typing.List) -> None:
-        """Do something after all hooks are executed.
-
-        By default it does nothing."""
-        pass
 
     async def _run_result_callback(self) -> typing.List:
         """Gather all results from all queues
@@ -190,7 +175,29 @@ class AbstractContentProvider:
             queue.put_nowait(data)
 
 
-class PeriodicContentProvider(AbstractContentProvider, ABC):
+class ConsistentDataProvider(AbstractContentProvider, ABC):
+    def __new__(cls, *args, **kwargs):
+        obj = super().__new__(cls, *args, kwargs)
+        if ConsistentDataProvider.result_callback != cls.result_callback:
+            obj._is_with_callback = True
+        return obj
+
+    async def get_content(self) -> object:
+        """Get content, not implemented.
+
+        Returns:
+            Data in any format.
+            """
+        raise NotImplementedError(f"Get_site of {self.__class__} not overridden")
+
+    async def result_callback(self, results: typing.List) -> None:
+        """Do something after all hooks are executed.
+
+        By default it does nothing."""
+        pass
+
+
+class PeriodicContentProvider(ConsistentDataProvider, ABC):
     """Periodically executes get_content every period seconds.
 
     Attributes:
@@ -227,7 +234,7 @@ class PeriodicContentProvider(AbstractContentProvider, ABC):
                 await asyncio.sleep(self.period)
 
 
-class BlockingContentProvider(AbstractContentProvider, ABC):
+class BlockingContentProvider(ConsistentDataProvider, ABC):
     """Class for providers that are too slow or have unavoidable blocking IO.
 
     Get_content and result_callback are executed in different thread to let it block.
@@ -276,39 +283,116 @@ class BlockingContentProvider(AbstractContentProvider, ABC):
 
 
 class ComplexContentProvider(AbstractContentProvider):
+    """Class for complex data providers like servers etc.
+
+    The idea behind this provider is that to send and receive data to provider form some
+    external source with a help of message queue system.
+
+    To get the message system after object creation, call get_message_system BEFORE RUNNING
+    AsyncParser.
+
+    The class should not be created in the runtime as there is no way to get the message system
+    inside AsyncParser.
+
+    If you want to somehow preprocess data, sent to provider by message system, you should
+    override method get_message_system(self, content).
+
+    If you want to somehow postrpocess results of hooks before sending to message system,
+    you should override method postprocess_result(self, results).
+
+    Attributes:
+        _input_queue: queue from external system to provider(from message system to provider).
+        _output_queue: queue from provider to external system(from provider to message system).
+        _message_system: instance of MessageSystem to handle all external system-provider
+            data transfer.
+    """
+
     def __new__(cls, *args, **kwargs):
         obj = AbstractContentProvider.__new__(cls, *args, **kwargs)
         obj._input_queue = None
         obj._output_queue = None
         obj._message_system = MessageSystem()
-
         return obj
 
     async def __aenter__(self):
+        """Initialise all in-loop attributes of class.
+
+        Message system is also fully initialized for the use here.
+        """
         kek = await super().__aenter__()
         self._input_queue, self._output_queue = await self._message_system.initialize()
-        return kek
 
-    def get_message_system(self):
+    def get_message_system(self) -> MessageSystem:
+        """Return the message system."""
         return self._message_system
 
-    async def cycle(self):
+    async def cycle(self) -> None:
+        """In endless cycle get content, preprocess, send to hook, receive res, postprocess and send."""
         async with self:
             while True:
-                content, msg_id = await ComplexContentProvider.get_content(self)
+                content, msg_id = await self._input_queue.get()
+                content = await self.preprocess_data(content)
                 await self._notify_all_hooks(content)
+
                 result = await self._run_result_callback()
-                await ComplexContentProvider.result_callback(self, (result, msg_id))
+                result = await self.postprocess_result(result)
+                await self._output_queue.put((result, msg_id))
 
-    async def get_content(self):
-        return await self._input_queue.get()
+    async def preprocess_data(self, content: object) -> object:
+        """Preprocess data.
 
-    async def result_callback(self, results):
-        await self._output_queue.put(results)
+        Executed every time there's new info received from message system.
+
+        Args:
+            content: data, sent to provider by message system.
+
+        Returns:
+            Preprocessed data in any form.
+        """
+        return content
+
+    async def postprocess_result(self, results: typing.List[object]) -> object:
+        """Postprocess hooks results.
+
+        Executed every time results from all hooks are gathered before sending to message system.
+
+        Args:
+            results: list of results from all hooks.
+
+        Returns:
+            Postprocessed results to send to message system.
+        """
+        return results
 
 
 class MessageSystem:
+    """Message system to transfer data between complex system and ComplexContentProvider.
+
+    Each message in message system has it's own id. The id is given to message when message to
+    provider is created. Later you can get provider's answer to that message by it's id.
+
+    It is thread-safe.
+
+    It's purpose is to transfer data between ComplexContentProvider and complex system.
+
+    Attributes:
+        _input_queue: asyncio.Queue from message system to provider.
+        _output_queue: asyncio.Queue from provider to message system.
+        _tmp_queue: queue.Queue object for temporal data transfer before AsyncParser starts.
+        _message_id: amount of sent messages, also serves as id to next message.
+        _asyncio_loop: asyncio.Loop, in which the MessageSystem works.
+        _result_to_mID_mapping: dict, used to store answers to messages by their id.
+        _needed_output_events: dict of threading.Event, stores events for needing answers for
+        messages that hasn't been yet.
+         _lock: threading.Lock for thread safe message sends.
+         _task: asyncio.Task for message's system work(cycle() coroutine).
+        """
+
     def __init__(self):
+        """Construct the object.
+
+        Almost nothing is initialized as almost all initialisations need to be done in event loop.
+        """
         self._input_queue = None
         self._output_queue = None
         self._tmp_queue = queue.Queue()
@@ -320,27 +404,45 @@ class MessageSystem:
         self._task = None
 
     async def initialize(self):
+        """Initialize everything when in event loop.
+
+        All messages that were sent to system before it are not lost.
+        """
         input_queue = asyncio.Queue()
         self._output_queue = asyncio.Queue()
         self._asyncio_loop = asyncio.get_event_loop()
+
         with self._lock:
             while not self._tmp_queue.empty():
                 msg = self._tmp_queue.get()
                 input_queue.put_nowait(msg)
             self._input_queue = input_queue
-        self._task = asyncio.create_task(self.cycle())
+
+        self._task = asyncio.create_task(self._serve_provider_results())
         return self._input_queue, self._output_queue
 
-    def send_to_provider(self, message):
+    def send_to_provider(self, data: object) -> int:
+        """Send message from system to provider.
+
+        Don't be afraid of sending messages before start of AsyncParser's work,
+        the messages will be remembered anyways.
+
+        Args:
+            data: data to send to provider, any type.
+
+        Returns:
+            Message's id.
+        """
         if self._input_queue is None:
-            self._tmp_queue.put_nowait((message, self._message_id))
+            self._tmp_queue.put_nowait((data, self._message_id))
         else:
-            asyncio.run_coroutine_threadsafe(self._input_queue.put((message, self._message_id)),
+            asyncio.run_coroutine_threadsafe(self._input_queue.put((data, self._message_id)),
                                              self._asyncio_loop)
         self._message_id += 1
         return self._message_id - 1
 
-    async def cycle(self):
+    async def _serve_provider_results(self) -> None:
+        """Serve provider results in endless cycle."""
         while True:
             result, msg_id = await self._output_queue.get()
             with self._lock:
@@ -348,18 +450,37 @@ class MessageSystem:
                 if msg_id in self._needed_output_events:
                     self._needed_output_events[msg_id].set()
 
-    def retrieve_result(self, message_id):
+    def retrieve_result(self, message_id: int) -> object:
+        """Get results from ComplexContentProvider.
+
+        Args:
+            message_id: Id of message, for which answer is needed.
+
+        Returns:
+            Answer with on message message_id.
+        """
         cur_event = threading.Event()
+
         with self._lock:
             if message_id in self._result_to_mID_mapping:
                 return self._result_to_mID_mapping[message_id]
+
             self._needed_output_events[message_id] = cur_event
         cur_event.wait()
+
         del self._needed_output_events[message_id]
         return self._result_to_mID_mapping[message_id]
 
-    def send_wait_answer(self, message):
-        msg_id = self.send_to_provider(message)
+    def send_wait_answer(self, data: object) -> object:
+        """Send message to provider and wait till result is sent back.
+
+        Args:
+            data: data to send.
+
+        Returns:
+            Result from provider.
+        """
+        msg_id = self.send_to_provider(data)
         return self.retrieve_result(msg_id)
 
 
