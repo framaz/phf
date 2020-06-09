a142b40e4b837e0c4fe4860d7faff39362135b53
framaz <framaz@yandex.ru>
2020-06-09 18:52:18 +0500

    test: group tests to classes

--- tests/test_hooks.py
+++ tests/test_hooks.py
@@ -3,61 +3,57 @@ import asyncio
 import pytest
 
 
-@pytest.mark.asyncio
-async def test_hook_action(hook_factory):
-    hook = await hook_factory.get_started_hook()
-    in_queue = hook.get_straight_queue()
-    out_queue = hook.get_callback_queue()
-    message = "test"
-    await in_queue.put(message)
-    res = await out_queue.get()
-    assert message == res
+class TestHooks:
+    @pytest.mark.asyncio
+    async def test_hook_action(self, hook_factory):
+        hook = await hook_factory.get_started_hook()
+        in_queue = hook.get_straight_queue()
+        out_queue = hook.get_callback_queue()
+        message = "test"
+        await in_queue.put(message)
+        res = await out_queue.get()
+        assert message == res
 
+    @pytest.mark.asyncio
+    async def test_hook_queues_creation_retrieve(self, hook):
+        in_queue = hook.get_straight_queue()
+        assert isinstance(in_queue, asyncio.Queue)
+        assert in_queue is hook.get_straight_queue()
+        out_queue = hook.get_callback_queue()
+        assert isinstance(out_queue, asyncio.Queue)
+        assert out_queue is hook.get_callback_queue()
 
-@pytest.mark.asyncio
-async def test_hook_queues_creation_retrieve(hook):
-    in_queue = hook.get_straight_queue()
-    assert isinstance(in_queue, asyncio.Queue)
-    assert in_queue is hook.get_straight_queue()
-    out_queue = hook.get_callback_queue()
-    assert isinstance(out_queue, asyncio.Queue)
-    assert out_queue is hook.get_callback_queue()
+    @pytest.mark.asyncio
+    async def test_alias_and_change(self, hook):
+        alias = hook.get_aliases()
+        assert alias == hook.__class__._alias
+        alias.append("azaza")
+        assert alias != hook.get_aliases()
 
+    @pytest.mark.asyncio
+    async def test_hook_no_init_call(self, hook):
+        assert "_asyncio_queue" in hook.__dict__
+        assert "_callback_queue" in hook.__dict__
+        assert "_provider" in hook.__dict__
 
-@pytest.mark.asyncio
-async def test_alias_and_change(hook):
-    alias = hook.get_aliases()
-    assert alias == hook.__class__._alias
-    alias.append("azaza")
-    assert alias != hook.get_aliases()
+    @pytest.mark.asyncio
+    async def test_hook_stopping(self, hook):
+        task = asyncio.Task(hook.cycle_call())
+        await asyncio.sleep(1)
+        hook.stop()
+        await hook.get_straight_queue().put("kek")
+        res = task
 
-
-@pytest.mark.asyncio
-async def test_hook_no_init_call(hook):
-    assert "_asyncio_queue" in hook.__dict__
-    assert "_callback_queue" in hook.__dict__
-    assert "_provider" in hook.__dict__
-
-
-@pytest.mark.asyncio
-async def test_hook_stopping(hook):
-    task = asyncio.Task(hook.cycle_call())
-    await asyncio.sleep(1)
-    hook.stop()
-    await hook.get_straight_queue().put("kek")
-    res = task
-
-
-@pytest.mark.asyncio
-async def test_multiple_unconsistent_add_and_retrieve(hook_factory):
-    hook = await hook_factory.get_started_hook()
-    in_queue = hook.get_straight_queue()
-    out_queue = hook.get_callback_queue()
-    results = []
-    in_queue.put_nowait(0)
-    in_queue.put_nowait(1)
-    results.append(await out_queue.get())
-    in_queue.put_nowait(2)
-    results.append(await out_queue.get())
-    results.append(await out_queue.get())
-    assert results == [0, 1, 2]
+    @pytest.mark.asyncio
+    async def test_multiple_unconsistent_add_and_retrieve(self, hook_factory):
+        hook = await hook_factory.get_started_hook()
+        in_queue = hook.get_straight_queue()
+        out_queue = hook.get_callback_queue()
+        results = []
+        in_queue.put_nowait(0)
+        in_queue.put_nowait(1)
+        results.append(await out_queue.get())
+        in_queue.put_nowait(2)
+        results.append(await out_queue.get())
+        results.append(await out_queue.get())
+        assert results == [0, 1, 2]
--- tests/test_providers.py
+++ tests/test_providers.py
@@ -5,198 +5,204 @@ import pytest
 from ProviderHookFramework.provider import BlockingContentProvider
 
 
-def test_aliases(any_abstract_provider):
-    assert any_abstract_provider.get_aliases() == \
-           any_abstract_provider.__class__._alias
+class TestBasicForAllProviders:
+    def test_aliases(self, any_abstract_provider):
+        assert any_abstract_provider.get_aliases() == \
+               any_abstract_provider.__class__._alias
 
+    def test_all_vars(self, any_abstract_provider):
+        assert "_asynio_hooks" in any_abstract_provider.__dict__
+        assert "_asyncio_hook_tasks" in any_abstract_provider.__dict__
+        assert "_asyncio_straight_queues" in any_abstract_provider.__dict__
+        assert "_asyncio_callback_queues" in any_abstract_provider.__dict__
+        assert "_asyncio_task" in any_abstract_provider.__dict__
+        assert "_asyncio_running" in any_abstract_provider.__dict__
+        assert "_asyncio_loop" in any_abstract_provider.__dict__
+        assert "_is_with_callback" in any_abstract_provider.__dict__
 
-def test_all_vars(any_abstract_provider):
-    assert "_asynio_hooks" in any_abstract_provider.__dict__
-    assert "_asyncio_hook_tasks" in any_abstract_provider.__dict__
-    assert "_asyncio_straight_queues" in any_abstract_provider.__dict__
-    assert "_asyncio_callback_queues" in any_abstract_provider.__dict__
-    assert "_asyncio_task" in any_abstract_provider.__dict__
-    assert "_asyncio_running" in any_abstract_provider.__dict__
-    assert "_asyncio_loop" in any_abstract_provider.__dict__
-    assert "_is_with_callback" in any_abstract_provider.__dict__
-
-
-def test_hook_addition_and_retrieve_nonstarted_provider(any_abstract_provider,
-                                                        hook):
-    any_abstract_provider.add_hook(hook)
-    assert any_abstract_provider._asyncio_straight_queues == []
-    assert any_abstract_provider.get_hooks() == [hook]
+    def test_hook_addition_and_retrieve_nonstarted_provider(self,
+                                                            any_abstract_provider,
+                                                            hook):
+        any_abstract_provider.add_hook(hook)
+        assert any_abstract_provider._asyncio_straight_queues == []
+        assert any_abstract_provider.get_hooks() == [hook]
 
 
 amount_of_hooks = [0, 1, 2]
 
 
-@pytest.mark.asyncio
-@pytest.mark.parametrize("hook_amount", amount_of_hooks)
-async def test_consistent_provider_workflow_hooks_addition_before_cycle(
-        hook_amount,
-        hook_factory,
-        any_nonabstract_consistent_provider,
-        capsys,
-        monkeypatch,
-        run_smth_once
-):
-    monkeypatch.setattr(any_nonabstract_consistent_provider,
-                        "_is_running",
-                        run_smth_once)
+class TestConsistentProviders:
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize("hook_amount", amount_of_hooks)
+    async def test_consistent_provider_workflow_hooks_addition_before_cycle(
+            self,
+            hook_amount,
+            hook_factory,
+            any_nonabstract_consistent_provider,
+            capsys,
+            monkeypatch,
+            run_smth_once
+    ):
+        monkeypatch.setattr(any_nonabstract_consistent_provider,
+                            "_is_running",
+                            run_smth_once)
 
-    for i in range(hook_amount):
-        hook = await hook_factory.get_hook()
-        any_nonabstract_consistent_provider.add_hook(hook)
-    await any_nonabstract_consistent_provider.cycle()
+        for i in range(hook_amount):
+            hook = await hook_factory.get_hook()
+            any_nonabstract_consistent_provider.add_hook(hook)
+        await any_nonabstract_consistent_provider.cycle()
 
-    if isinstance(any_nonabstract_consistent_provider, BlockingContentProvider):
-        await asyncio.sleep(0.2)
-    assert any_nonabstract_consistent_provider.logs[-1] == [0] * hook_amount
+        if isinstance(any_nonabstract_consistent_provider, BlockingContentProvider):
+            await asyncio.sleep(0.2)
+        assert any_nonabstract_consistent_provider.logs[-1] == [0] * hook_amount
 
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize("hook_amount", amount_of_hooks)
+    async def test_consistent_provider_workflow_hooks_addition_after_cycle(
+            self,
+            hook_amount,
+            hook_factory,
+            controlled_result_callback_provider
+    ):
+        any_nonabstract_consistent_provider, control_func = \
+            controlled_result_callback_provider
+        task = asyncio.Task(any_nonabstract_consistent_provider.cycle())
 
-@pytest.mark.asyncio
-@pytest.mark.parametrize("hook_amount", amount_of_hooks)
-async def test_consistent_provider_workflow_hooks_addition_after_cycle(
-        hook_amount,
-        hook_factory,
-        controlled_result_callback_provider
-):
-    any_nonabstract_consistent_provider, control_func = \
-        controlled_result_callback_provider
-    task = asyncio.Task(any_nonabstract_consistent_provider.cycle())
+        for i in range(hook_amount):
+            hook = await hook_factory.get_hook()
+            any_nonabstract_consistent_provider.add_hook(hook)
 
-    for i in range(hook_amount):
-        hook = await hook_factory.get_hook()
-        any_nonabstract_consistent_provider.add_hook(hook)
-
-    await control_func()
-    assert any_nonabstract_consistent_provider.logs[-1] == [0] * hook_amount
-
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize("hook_amount", amount_of_hooks)
-async def test_multiple_in_order(
-        hook_amount,
-        hook_factory,
-        controlled_result_callback_provider
-):
-    times = 3
-    any_nonabstract_consistent_provider, control_func = \
-        controlled_result_callback_provider
-    task = asyncio.Task(any_nonabstract_consistent_provider.cycle())
-
-    for i in range(hook_amount):
-        hook = await hook_factory.get_hook()
-        any_nonabstract_consistent_provider.add_hook(hook)
-
-    expected_res = []
-    for i in range(times):
         await control_func()
-        expected_res.append([i] * hook_amount)
+        assert any_nonabstract_consistent_provider.logs[-1] == [0] * hook_amount
 
-    assert any_nonabstract_consistent_provider.logs == expected_res
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize("hook_amount", amount_of_hooks)
+    async def test_multiple_in_order(
+            self,
+            hook_amount,
+            hook_factory,
+            controlled_result_callback_provider
+    ):
+        times = 3
+        any_nonabstract_consistent_provider, control_func = \
+            controlled_result_callback_provider
+        task = asyncio.Task(any_nonabstract_consistent_provider.cycle())
 
+        for i in range(hook_amount):
+            hook = await hook_factory.get_hook()
+            any_nonabstract_consistent_provider.add_hook(hook)
 
-@pytest.mark.asyncio
-async def test_message_system_uninit_message_queueing(message_system):
-    msg_amount = 3
-    for i in range(msg_amount):
-        message_system.send_to_provider(i + 1)
+        expected_res = []
+        for i in range(times):
+            await control_func()
+            expected_res.append([i] * hook_amount)
 
-    assert message_system._tmp_queue.qsize() == msg_amount
-
-    await message_system.initialize()
-
-    assert message_system._tmp_queue.qsize() == 0
-    assert message_system._input_queue.qsize() == msg_amount
-
-    for i in range(3):
-        assert (await message_system._input_queue.get())[0] == i + 1
-
-
-@pytest.mark.asyncio
-async def test_message_system_message_and_answer(message_system):
-    await message_system.initialize()
-    for i in range(3):
-        message_system.send_to_provider(i + 1)
-
-    for i in range(3):
-        msg = await message_system._input_queue.get()
-        assert isinstance(msg, tuple)
-        assert msg[1] == i
-        assert msg[0] == i + 1
-        await message_system._output_queue.put(msg)
-
-    await asyncio.sleep(1)  # needed as everything is executed in the same loop
-
-    assert message_system.retrieve_result(1) == 2
-    assert message_system.retrieve_result(2) == 3
-    assert message_system.retrieve_result(0) == 1
+        assert any_nonabstract_consistent_provider.logs == expected_res
 
 
-def test_nonstarted_complex_provider(complex_provider_nonstarted):
-    message_system = complex_provider_nonstarted.get_message_system()
-    message_system.send_to_provider(0)
+class TestComplexContentProvider:
+    class TestMessageSystem:
+        @pytest.mark.asyncio
+        async def test_message_system_uninit_message_queueing(self,
+                                                              message_system):
+            msg_amount = 3
+            for i in range(msg_amount):
+                message_system.send_to_provider(i + 1)
 
-    assert message_system._tmp_queue.qsize() == 1
-    assert message_system._input_queue is None
-    assert message_system._output_queue is None
+            assert message_system._tmp_queue.qsize() == msg_amount
 
+            await message_system.initialize()
 
-@pytest.mark.asyncio
-@pytest.mark.parametrize("hook_amount", amount_of_hooks)
-async def test_complex_provider(complex_provider, hook_factory, hook_amount):
-    for i in range(hook_amount):
-        hook = await hook_factory.get_hook()
-        complex_provider.add_hook(hook)
+            assert message_system._tmp_queue.qsize() == 0
+            assert message_system._input_queue.qsize() == msg_amount
 
-    message_system = complex_provider.get_message_system()
-    for i in range(3):
-        res = message_system.send_wait_answer(i + 1)
-        assert res == [i + 1] * hook_amount
+            for i in range(3):
+                assert (await message_system._input_queue.get())[0] == i + 1
 
+        @pytest.mark.asyncio
+        async def test_message_system_message_and_answer(self,
+                                                         message_system):
+            await message_system.initialize()
+            for i in range(3):
+                message_system.send_to_provider(i + 1)
 
-@pytest.mark.asyncio
-@pytest.mark.parametrize("hook_amount", amount_of_hooks)
-async def test_complex_provider_nonconsistent_message_order(complex_provider,
-                                                            hook_factory,
-                                                            hook_amount):
-    for i in range(hook_amount):
-        hook = await hook_factory.get_hook()
-        complex_provider.add_hook(hook)
+            for i in range(3):
+                msg = await message_system._input_queue.get()
+                assert isinstance(msg, tuple)
+                assert msg[1] == i
+                assert msg[0] == i + 1
+                await message_system._output_queue.put(msg)
 
-    message_system = complex_provider.get_message_system()
-    for i in range(3):
-        msg_id = message_system.send_to_provider(i + 1)
-        assert msg_id == i
+            await asyncio.sleep(1)  # needed as everything is executed in the same loop
 
-    assert message_system.retrieve_result(1) == [2] * hook_amount
-    assert message_system.retrieve_result(2) == [3] * hook_amount
-    assert message_system.retrieve_result(0) == [1] * hook_amount
+            assert message_system.retrieve_result(1) == 2
+            assert message_system.retrieve_result(2) == 3
+            assert message_system.retrieve_result(0) == 1
 
+    def test_nonstarted_complex_provider(self, complex_provider_nonstarted):
+        message_system = complex_provider_nonstarted.get_message_system()
+        message_system.send_to_provider(0)
 
-@pytest.mark.slow_test
-@pytest.mark.asyncio
-@pytest.mark.parametrize("hook_amount", amount_of_hooks)
-async def test_long_working_hook(complex_provider,
-                                 hook_factory,
-                                 hook_amount,
-                                 monkeypatch):
-    async def _long_hook_action(data):
-        await asyncio.sleep(3)
-        return data
+        assert message_system._tmp_queue.qsize() == 1
+        assert message_system._input_queue is None
+        assert message_system._output_queue is None
 
-    for i in range(hook_amount):
-        hook = await hook_factory.get_hook()
-        monkeypatch.setattr(hook, "hook_action", _long_hook_action)
-        complex_provider.add_hook(hook)
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize("hook_amount", amount_of_hooks)
+    async def test_complex_provider(self,
+                                    complex_provider,
+                                    hook_factory,
+                                    hook_amount):
+        for i in range(hook_amount):
+            hook = await hook_factory.get_hook()
+            complex_provider.add_hook(hook)
 
-    message_system = complex_provider.get_message_system()
-    for i in range(3):
-        msg_id = message_system.send_to_provider(i + 1)
-        assert msg_id == i
+        message_system = complex_provider.get_message_system()
+        for i in range(3):
+            res = message_system.send_wait_answer(i + 1)
+            assert res == [i + 1] * hook_amount
 
-    assert message_system.retrieve_result(1) == [2] * hook_amount
-    assert message_system.retrieve_result(2) == [3] * hook_amount
-    assert message_system.retrieve_result(0) == [1] * hook_amount
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize("hook_amount", amount_of_hooks)
+    async def test_complex_provider_nonconsistent_message_order(self,
+                                                                complex_provider,
+                                                                hook_factory,
+                                                                hook_amount):
+        for i in range(hook_amount):
+            hook = await hook_factory.get_hook()
+            complex_provider.add_hook(hook)
+
+        message_system = complex_provider.get_message_system()
+        for i in range(3):
+            msg_id = message_system.send_to_provider(i + 1)
+            assert msg_id == i
+
+        assert message_system.retrieve_result(1) == [2] * hook_amount
+        assert message_system.retrieve_result(2) == [3] * hook_amount
+        assert message_system.retrieve_result(0) == [1] * hook_amount
+
+    @pytest.mark.slow_test
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize("hook_amount", amount_of_hooks)
+    async def test_long_working_hook(self,
+                                     complex_provider,
+                                     hook_factory,
+                                     hook_amount,
+                                     monkeypatch):
+        async def _long_hook_action(data):
+            await asyncio.sleep(3)
+            return data
+
+        for i in range(hook_amount):
+            hook = await hook_factory.get_hook()
+            monkeypatch.setattr(hook, "hook_action", _long_hook_action)
+            complex_provider.add_hook(hook)
+
+        message_system = complex_provider.get_message_system()
+        for i in range(3):
+            msg_id = message_system.send_to_provider(i + 1)
+            assert msg_id == i
+
+        assert message_system.retrieve_result(1) == [2] * hook_amount
+        assert message_system.retrieve_result(2) == [3] * hook_amount
+        assert message_system.retrieve_result(0) == [1] * hook_amount
