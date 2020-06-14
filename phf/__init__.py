8fbcb78adf0d0c98c1e390fa337c6e33e68fc1ef
framaz <framaz@yandex.ru>
2020-06-14 17:33:51 +0500

    feat: fill __init__ of phf.

---
+++ phf/__init__.py
@@ -0,0 +1,7 @@
+from . import abstracthook
+from . import commandinput
+from . import provider
+from . import utils
+from .phfsystem import PHFSystem
+
+__all__ = ["abstracthook", "commandinput", "provider", "utils", "PHFSystem"]
